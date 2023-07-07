a75e2f8915902de5e6013a1e49eee2af217be69e
Shaun Prince <suparious@solidrust.net>
2023-07-06 21:00:51 -0700

    update default models to the latest available

--- server/config.py
+++ server/config.py
@@ -1,8 +1,8 @@
 models = {
     'text-gpt-0040-render-sha-0': 'gpt-4',
     'text-gpt-0035-render-sha-0': 'gpt-3.5-turbo',
-    'text-gpt-0035-render-sha-0301': 'gpt-3.5-turbo-0613',
-    'text-gpt-0040-render-sha-0314': 'gpt-4-0613',
+    'text-gpt-0035-render-sha-0613': 'gpt-3.5-turbo-0613',
+    'text-gpt-0040-render-sha-0613': 'gpt-4-0613',
 }
 
 special_instructions = {
